97d8a80563f8f57ec788d8961a14817e2fc870d4
qseft0402 <qseft0402@gmail.com>
2026-04-14 21:13:23 +0800

    20260414

--- GUI_Class_2026/GUI_Class_2026/Form1.Designer.cs
+++ GUI_Class_2026/GUI_Class_2026/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.btn_ChangeLabel = new System.Windows.Forms.Button();
             this.btn_Counter = new System.Windows.Forms.Button();
             this.lab_Counter = new System.Windows.Forms.Label();
+            this.btnBigger = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -98,11 +104,57 @@
             this.lab_Counter.TabIndex = 4;
             this.lab_Counter.Text = "0";
             // 
+            // btnBigger
+            // 
+            this.btnBigger.Location = new System.Drawing.Point(282, 96);
+            this.btnBigger.Name = "btnBigger";
+            this.btnBigger.Size = new System.Drawing.Size(146, 65);
+            this.btnBigger.TabIndex = 6;
+            this.btnBigger.Text = "按鈕放大";
+            this.btnBigger.UseVisualStyleBackColor = true;
+            this.btnBigger.Click += new System.EventHandler(this.btnBigger_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(504, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "按我複製至串到Label";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(502, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBigger);
             this.Controls.Add(this.btn_Counter);
             this.Controls.Add(this.lab_Counter);
             this.Controls.Add(this.btn_ChangeLabel);
@@ -112,6 +164,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +178,11 @@
         private System.Windows.Forms.Button btn_ChangeLabel;
         private System.Windows.Forms.Button btn_Counter;
         private System.Windows.Forms.Label lab_Counter;
+        private System.Windows.Forms.Button btnBigger;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GUI_Class_2026/GUI_Class_2026/Form1.cs
+++ GUI_Class_2026/GUI_Class_2026/Form1.cs
@@ -41,5 +41,22 @@ namespace GUI_Class_2026
             int sum = Int32.Parse(count)+1;
             lab_Counter.Text = sum.ToString();
         }
+
+        private void btnBigger_Click(object sender, EventArgs e)
+        {
+            new FormBtnBigger().Show();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            label2.Text= str;
+        }
     }
 }
